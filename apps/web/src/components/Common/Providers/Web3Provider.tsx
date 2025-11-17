1a01b76b04c6e409b722f2a4bcca13fefd4b811a
Minh Hieu Tran <hieutm.site@gmail.com>
2025-11-17 11:10:34 +0700

    config(rpcs): add bsc rpc to provider

--- apps/web/src/components/Common/Providers/Web3Provider.tsx
+++ apps/web/src/components/Common/Providers/Web3Provider.tsx
@@ -1,11 +1,14 @@
 import {
   CHAIN,
+  IS_MAINNET,
   WALLETCONNECT_PROJECT_ID
 } from "@slice/data/constants";
 import { familyAccountsConnector } from "family";
 import type { ReactNode } from "react";
 import { createConfig, WagmiProvider } from "wagmi";
 import { injected, walletConnect } from "wagmi/connectors";
+import { bscTestnet, bsc } from "wagmi/chains";
+import { http } from "viem";
 import getRpc from "@/helpers/getRpc";
 
 const connectors = [
@@ -14,11 +17,15 @@ const connectors = [
   injected()
 ];
 
+// Thêm BSC chain để hỗ trợ bridge
+const BSC_CHAIN = IS_MAINNET ? bsc : bscTestnet;
+
 const config = createConfig({
-  chains: [CHAIN],
+  chains: [CHAIN, BSC_CHAIN],
   connectors,
   transports: {
-    [CHAIN.id]: getRpc({ chainId: CHAIN.id })
+    [CHAIN.id]: getRpc({ chainId: CHAIN.id }),
+    [BSC_CHAIN.id]: getRpc({ chainId: BSC_CHAIN.id })
   }
 });
 
