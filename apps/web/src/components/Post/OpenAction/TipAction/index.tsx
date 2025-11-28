f3e83e589cd0342f19750673d6a3b3015cc64fc0
Quách Tuấn Khang <khangcm24@gmail.com>
2025-11-28 15:15:25 +0700

    cannot tip your own post

--- apps/web/src/components/Post/OpenAction/TipAction/index.tsx
+++ apps/web/src/components/Post/OpenAction/TipAction/index.tsx
@@ -7,6 +7,7 @@ import TipMenu from "@/components/Shared/TipMenu";
 import { Tooltip } from "@/components/Shared/UI";
 import cn from "@/helpers/cn";
 import stopEventPropagation from "@/helpers/stopEventPropagation";
+import { useAccountStore } from "@/store/persisted/useAccountStore";
 
 interface TipActionProps {
   post: PostFragment;
@@ -14,14 +15,18 @@ interface TipActionProps {
 }
 
 const TipAction = ({ post, showCount }: TipActionProps) => {
+  const { currentAccount } = useAccountStore();
   const hasTipped = post.operations?.hasTipped;
-  const  tips  = post.stats.tips || 0;
+  const tips = post.stats.tips || 0;
 
   const iconClassName = showCount
     ? "w-[17px] sm:w-[20px]"
     : "w-[15px] sm:w-[18px]";
 
   const hasVisibleCount = tips > 0 && !showCount;
+
+  // Disable tip nếu là bài đăng của chính user
+  const isOwnPost = currentAccount?.address === post?.author?.address;
 
   return (
     <div
@@ -33,25 +38,32 @@ const TipAction = ({ post, showCount }: TipActionProps) => {
       <Menu as="div" className="relative">
         <MenuButton
           aria-label="Tip"
-          className="rounded-full p-1.5 outline-offset-2 hover:bg-gray-300/20 dark:hover:bg-gray-700/40"
+          className={cn(
+            "rounded-full p-1.5 outline-offset-2",
+            isOwnPost 
+              ? "cursor-not-allowed opacity-50" 
+              : "hover:bg-gray-300/20 dark:hover:bg-gray-700/40"
+          )}
+          disabled={isOwnPost}
           onClick={stopEventPropagation}
         >
-          <Tooltip content="Tip" placement="top" withDelay>
+          <Tooltip content={isOwnPost ? "Cannot tip your own post" : "Tip"} placement="top" withDelay>
             <TipIcon className={cn("post-action-icon", iconClassName)} />
           </Tooltip>
         </MenuButton>
-        {/* Menu giữ nguyên */}
-        <MenuTransition>
-          <MenuItems
-            anchor="bottom start"
-            className="z-[5] mt-2 w-max origin-top-left rounded-xl border border-gray-200 bg-white shadow-xs focus:outline-hidden dark:border-gray-700 dark:bg-[#121212]"
-            static
-          >
-            <MenuItem>
-              {({ close }) => <TipMenu closePopover={close} post={post} />}
-            </MenuItem>
-          </MenuItems>
-        </MenuTransition>
+        {!isOwnPost && (
+          <MenuTransition>
+            <MenuItems
+              anchor="bottom start"
+              className="z-[5] mt-2 w-max origin-top-left rounded-xl border border-gray-200 bg-white shadow-xs focus:outline-hidden dark:border-gray-700 dark:bg-[#121212]"
+              static
+            >
+              <MenuItem>
+                {({ close }) => <TipMenu closePopover={close} post={post} />}
+              </MenuItem>
+            </MenuItems>
+          </MenuTransition>
+        )}
       </Menu>
 
       <span className="post-action-count text-gray-500 dark:text-gray-200">
