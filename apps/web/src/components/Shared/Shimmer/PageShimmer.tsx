15465f63fa33c0929d632e4a8f4a3a28b97850d1
Quách Tuấn Khang <khangcm24@gmail.com>
2025-12-01 20:39:55 +0700

    update skeleton newpost and tabs

--- apps/web/src/components/Shared/Shimmer/PageShimmer.tsx
+++ apps/web/src/components/Shared/Shimmer/PageShimmer.tsx
@@ -8,12 +8,20 @@ const PageShimmer = () => {
     <PageLayout>
       {/* Skeleton cho tabs */}
       <div className="flex justify-center px-3">
-        <Skeleton className="h-10 w-64 rounded-full" />
+        <div className="h-10 w-64 rounded-full bg-white dark:bg-black border border-gray-200 dark:border-gray-700 flex items-center justify-center gap-2 px-4">
+          <Skeleton className="h-6 w-20 rounded-lg" />
+          <Skeleton className="h-6 w-20 rounded-lg" />
+        </div>
       </div>
       
       {/* Skeleton cho NewPost composer */}
       <div className="px-3">
-        <Skeleton className="h-[68px] w-full rounded-xl" />
+        <div className="h-[68px] w-full rounded-xl bg-white dark:bg-black border border-gray-200 dark:border-gray-700 px-5 pt-4 pb-3">
+          <div className="flex items-center space-x-3">
+            <Skeleton className="size-11 rounded-full" />
+            <Skeleton className="h-5 w-32 rounded-lg" />
+          </div>
+        </div>
       </div>
       
       <PostsShimmer />
