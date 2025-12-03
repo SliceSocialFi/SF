4b7f12ca78a623519a0ac668f293caf57a95afc6
Tmh3101 <minhhieu31012004@gmail.com>
2025-12-03 18:03:56 +0700

    remove vars log

--- packages/data/constants.ts
+++ packages/data/constants.ts
@@ -79,12 +79,6 @@ export const PERMISSIONS = {
 export const BRIDGE_API_URL = process.env.BRIDGE_API_URL || "http://localhost:8787/";
 export const PAYMENT_API_URL = process.env.PAYMENT_API_URL || "http://localhost:3000/";
 
-console.log("process.env.BRIDGE_API_URL:", process.env.BRIDGE_API_URL);
-console.log("Using BRIDGE API URL:", BRIDGE_API_URL);
-
-console.log("process.env.PAYMENT_API_URL:", process.env.PAYMENT_API_URL);
-console.log("Using Payment API URL:", PAYMENT_API_URL);
-
 export const BSC_POOL_CONTRACT =
   IS_MAINNET
     ? MAINNET_CONTRACTS.bscPool
